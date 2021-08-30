fd02543cf791f2d2660ee69410d781f9eb87ce03
dphogit <deanp21@gmail.com>
2021-08-30 21:20:53 +1200

    blog page fetch config setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,4 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
   plugins: [
     `gatsby-plugin-material-ui`,
     `gatsby-plugin-image`,
@@ -17,6 +10,28 @@ module.exports = {
         rule: {
           include: /assets/,
         },
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1080,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
       },
     },
   ],
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,53 @@
 import React from "react"
 import { Typography } from "@material-ui/core"
+import { graphql, useStaticQuery, Link } from "gatsby"
+import { getSrc } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              date
+              blurb
+              indexImage {
+                name
+                childImageSharp {
+                  gatsbyImageData(width: 600)
+                }
+              }
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <Typography variant="h1">Blog Page</Typography>
       <Typography>This is my blog page!</Typography>
+      {data.allMarkdownRemark.edges.map(edge => {
+        const { frontmatter } = edge.node
+        const { indexImage } = frontmatter
+        return (
+          <Link to={`/blog/${edge.node.fields.slug}`} key={edge.node.id}>
+            <p>{frontmatter.date}</p>
+            <h5>{frontmatter.title}</h5>
+            <p>{frontmatter.blurb}</p>
+            <img src={getSrc(frontmatter.indexImage)} alt={indexImage.name} />
+          </Link>
+        )
+      })}
     </Layout>
   )
 }
